e600d96bf4058511aa8a98c71c71399b53c654ea
Ty Everton <everton_915@yahoo.com>
2021-12-29 17:37:03 -0700

    adding tool belt icons

--- src/Components/ToolBelt.js
+++ src/Components/ToolBelt.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import './toolbelt.css'
-import { AiFillHtml5 } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaHtml5 } from 'react-icons/fa'
+import { FaReact } from 'react-icons/fa'
+import { FaGitSquare } from 'react-icons/fa'
+import { FaCss3Alt } from 'react-icons/fa'
+import { FaJsSquare } from 'react-icons/fa'
+import { FaNodeJs } from 'react-icons/fa'
+
 
 const ToolBelt = () => {
-  render() {
-    return {
-      
-    }
-  }
+  return (
+    <div>
+      <h3 className = "toolBeltIcons">
+        <FaGithub />
+        <FaHtml5/>
+        <FaReact />
+        <FaGitSquare />
+        <FaCss3Alt />
+        <FaJsSquare />
+        <FaNodeJs />
+
+      </h3>
+    </div>
+  )
 }
 
 export default ToolBelt
