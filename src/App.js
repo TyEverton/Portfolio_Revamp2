ec409dc8e151d2484eb9b0079bf3dce8ee2522cf
Ty Everton <everton_915@yahoo.com>
2022-01-03 14:35:43 -0700

    added import react line

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
+import { React } from 'react'
 import ContactForm from './Components/ContactForm'
-import ToolBelt from "./Components/ToolBelt"
-import ResumeDownload from "./Components/ResumeDownload"
-import AboutMe from "./Components/AboutMe"
-import NavBar from "./Components/NavBar"
-import Projects from "./Components/Projects"
+import ToolBelt from './Components/ToolBelt'
+import ResumeDownload from './Components/ResumeDownload'
+import AboutMe from './Components/AboutMe'
+import NavBar from './Components/NavBar'
+import Projects from './Components/Projects'
 
 function App() {
   return (
