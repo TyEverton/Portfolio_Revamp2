7605d5b0af7c7bec68829e15f0c3cca466539879
Ty Everton <everton_915@yahoo.com>
2022-01-09 10:32:05 -0700

    adding a test paragraph to see if the changes are reflected to the live site

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -17,7 +17,10 @@ const AboutMe = () => {
           dig deeper allows me to get an accurate idea of what the client is
           wanting and needing. As a recent graduate of Devmountain's Web
           Development Boot Camp, I am ready and excited to break into the
-          industry and make a positive impact!
+          industry and make a positive impact! 
+        </p>
+        <p>
+          testing 123
         </p>
       </section>
     </div>
